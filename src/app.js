a5f5262219ba9b97b18d9bba42038c3f45bb0056
Muhammed Ishaq <muhammedishaqjamb20@gmail.com>
2026-08-19 13:31:38 +0100

    feat(cors): add Vercel origin and localhost for development

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ app.use(helmet());
 // CORS
 app.use(cors({
   origin: [
-    'https://muaishaq-academy-all-in-one.netlify.app'
+    'https://mfa-frontend-ten.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:5173'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
